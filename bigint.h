ead33bd932de202fb33bf92d74db25670ee3c4be
Daniel Kramer <43589646+DanielAKramer@users.noreply.github.com>
2018-09-25 18:43:22 -0400

    Create bigint.h

---
+++ bigint.h
@@ -0,0 +1,37 @@
+/*
+	Daniel Kramer
+	CS23001
+	Bigint.hpp
+*/
+
+#ifndef BIGINT_HPP
+#define BIGINT_HPP
+
+#include <iostream>
+
+const int MAX_SIZE = 256;
+
+class bigint
+{
+public:
+	//Constructors
+	bigint();
+	bigint(int);
+	bigint(const char[]);
+
+	//Overload operators
+	bool operator==(const bigint&) const;
+	bigint operator+(bigint) const;
+	int& operator[](int);
+	int operator[](int) const;
+
+	//friend functions
+	friend std::ostream& operator<<(std::ostream&, const bigint&);
+	friend std::istream& operator>>(std::istream&, bigint&);
+
+private:
+	int number[MAX_SIZE];
+};
+
+
+#endif
