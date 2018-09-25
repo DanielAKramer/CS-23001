a62533b5a2456b4c25f0d2caeb2c80462a797250
Daniel Kramer <43589646+DanielAKramer@users.noreply.github.com>
2018-09-25 19:21:06 -0400

    Create utilities.hpp

---
+++ Stack/utilities.hpp
@@ -0,0 +1,23 @@
+/*
+	Daniel Kramer
+	CS23001
+	Utilities.hpp
+*/
+
+#ifndef UTILITIES_HPP
+#define UTILITIES_HPP
+
+#include "stack.h"
+#include "string.h"
+#include <vector>
+
+
+//free function that converts infix to postfix
+void infToPostf(std::ifstream&, std::ofstream&);
+void toAssembly(std::ifstream&, std::ofstream&);
+String assemblyOperator(const String&);
+String iToS(int);
+String getToken(std::ifstream&);
+
+
+#endif
